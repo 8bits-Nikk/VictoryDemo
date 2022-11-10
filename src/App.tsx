02b18bbf797ca39103e912ee5abf4d24b54113fc
8bits-Nikk <nikhilmakwana028@gmail.com>
2022-11-10 16:53:25 +0530

    graph

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
-import {SafeAreaView} from 'react-native';
+import BarGraph from './screens/BarGraph';
 
+export type MainStackType = {
+  BarGraph: any;
+};
+const Stack = createNativeStackNavigator<MainStackType>();
 const App = () => {
-  return <SafeAreaView></SafeAreaView>;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name={'BarGraph'} component={BarGraph} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default App;
